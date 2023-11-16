b6cd9c7ccfe4a3ae698c659c7de89bbcb354d8ab
imran <imransk470gmail.com>
2023-11-16 12:19:23 -0600

    jhas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { DatabaseModule } from './Database/db.module';
 import { GptsDirectory } from './GptsDirectory/GptsDirectory.module';
 
 @Module({
-  imports: [DatabaseModule, GptsDirectory],
+  imports: [ConfigModule.forRoot(), DatabaseModule, GptsDirectory],
   controllers: [],
   providers: [],
 })
