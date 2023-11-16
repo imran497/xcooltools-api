80b2c8af8391ed785cc64b6a4ab4240a3605e463
imran <imransk470gmail.com>
2023-11-16 12:01:19 -0600

    test

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
   app.enableCors({
-    origin: ['https://xcool.tools', 'http://xcool.tools'],
-    methods: 'GET,POST',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    origin: configService.get('ALLOW_ORIGINS', 'http://localhost:3000'),
   });
-  console.log(process.env.PORT, 'This is the port received');
-  await app.listen(process.env.PORT || 3001);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  await app.listen(configService.get('PORT', '3001'), () =>
+    console.log('App listening on ' + configService.get('PORT', '3001')),
+  );
 }
 bootstrap();
